ebf5fe90d41ac40e2b0d1cb8c62315bd33f7450d
andrewgremlich <andrew.gremlich@gmail.com>
2016-05-03 11:09:45 -0600

    A reset of this version.  I don't what to use Nodejs just yet because then that would probably mean I need to use browserify.  I need a quick solution and I am going a different route through ES6

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-var path = require("path"),
-    args = process.argv;
+var path = require("path")
+,   colorThief = require("color-thief")
+,   fs = require("fs")
+,   args = process.argv;
 
-console.log(args);
+console.log(colorThief);
 
-//for (var i = 0; i < args.length; i++) {
-//    console.log(args[i], path.basename(args[i]));
-//}
+
+
 
 //node app.js \\iDrive\CurrDevMedia\Projects\Courses\AMFD101\Banner\amfd-101.jpg
-
-var link = 'http://mkweb.bcgsc.ca/color-summarizer/?url=static.flickr.com/37/88847543_d1eb68c5b9_m.jpg&precision=low&json=1';
-console.log(link);
